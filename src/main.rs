10262a69f335b63ff09455801bcd20a58399a974
ｳﾀｶﾀｷｮｳｽｲ <c1101081@planet.kanazawa-it.ac.jp>
2025-03-01 21:09:41 +0000

    Renderへのでデプロイ準備

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,34 @@ use axum::{
 
 #[tokio::main]
 async fn main() {
+    let port_key = "PORT";
+    let default_port = 3000;
+    let port = match std::env::var(port_key) {
+        Ok(val) => match val.parse::<u16>() {
+            Ok(port) => port,
+            Err(_) => {
+                println!(
+                    "the port number \"{}\" is invalid. default port will be used.",
+                    val
+                );
+                default_port
+            }
+        },
+        Err(_) => {
+            println!(
+                "\"{}\" is not defined in environment variables. default port will be used.",
+                port_key
+            );
+            default_port
+        }
+    };
+
     let app = Router::new()
         .route("/login", get(login))
         .route("/callback", get(callback))
         .route("/logout", get(logout));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}",port)).await.unwrap();
     axum::serve(listener, app)
         .await
         .unwrap();
